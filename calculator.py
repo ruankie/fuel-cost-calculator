39b6de05e0805f879c30813c623acfb62fd9ebe9
Ruan Pretorius <pretoriusr.7@gmail.com>
2022-09-06 18:40:44 +0200

    made executable

--- calculator.py
+++ calculator.py
@@ -1,3 +1,6 @@
+import argparse
+
+
 class FuelCostCalculator:
     def __init__(
         self,
@@ -15,3 +18,36 @@ class FuelCostCalculator:
         """Return cost (in base currency) per km driven."""
         efficiency = self.tank_size / self.distance_on_tank  # L/km
         return self.fuel_cost * efficiency  # ZAR/km
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-t", "--tank", type=float, help="tank size in L", required=True
+    )
+    parser.add_argument(
+        "-d",
+        "--distance",
+        type=float,
+        help="distance traveled on full tank in km",
+        required=True,
+    )
+    parser.add_argument(
+        "-f",
+        "--fuel",
+        type=float,
+        help="fuel cost in base currency per L",
+        required=True,
+    )
+    parser.add_argument("-b", "--base", help="base currency symbol", default="ZAR")
+    args = parser.parse_args()
+
+    calc = FuelCostCalculator(
+        tank_size=args.tank,
+        distance_on_tank=args.distance,
+        fuel_cost=args.fuel,
+        base_currency=args.base,
+    )
+
+    cost = calc.calculate_cost_per_km()
+    print(f"Cost per km: {calc.base_currency} {round(cost, 2)}")
